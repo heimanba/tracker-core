82a795d3719fe9412d10c59765f9bbc47853831c
heimanba <yuankun.yk@alibaba-inc.com>
2022-07-05 17:50:52 +0800

    fix: spm值

--- src/plugins/spm.ts
+++ src/plugins/spm.ts
@@ -1,6 +1,11 @@
 import Url from "url-parse";
 import { getHost } from "../lib";
 
+interface IConfig {
+  whileAttr?: string;
+  getTrackerType?: any;
+}
+
 const getRedirectUrl = (url) => {
   if (!url) return "";
   const { hostname, pathname, hash } = new Url(url);
@@ -11,6 +16,9 @@ const parseTrackerAttr = (attr: string) => {
   if (!attr) return {};
   const attrList = attr.split("&");
   const attrMap = {};
+  if (attrList.length <= 1) {
+    return attrMap;
+  }
   attrList.forEach((item) => {
     const list = item.split("=");
     if (list.length > 1) {
@@ -22,7 +30,7 @@ const parseTrackerAttr = (attr: string) => {
   return attrMap;
 };
 
-const htmlTreeToStr = function (rootNode) {
+const htmlTreeToStr = function (rootNode, config: IConfig) {
   if (!(rootNode && rootNode.nodeType === 1)) return "";
   let currentElem = rootNode || null;
   const MAX_TRAVERSE_HEIGHT = 45;
@@ -32,8 +40,8 @@ const htmlTreeToStr = function (rootNode) {
   };
   let height = 0;
   while (currentElem && height++ < MAX_TRAVERSE_HEIGHT) {
-    const nextAttrStr = htmlNodeToStr(currentElem, true);
-    const nextALinkStr = htmlNodeToStr(currentElem, false);
+    const nextAttrStr = htmlNodeToStr(currentElem, config, true);
+    const nextALinkStr = htmlNodeToStr(currentElem, config, false);
     if (nextAttrStr) {
       out.attr.push(nextAttrStr);
     }
@@ -46,16 +54,19 @@ const htmlTreeToStr = function (rootNode) {
   return out;
 };
 
-const htmlNodeToStr = function (htmlNode, isAttr) {
+const htmlNodeToStr = function (htmlNode, config: IConfig, isAttr) {
   let out = null;
+  const { whileAttr } = config;
   if (!htmlNode || typeof htmlNode.tagName !== "string") {
     return null;
   }
-  const attrWhitelist = ["data-tracker", "data-wrapper"];
+  const myAttrWhitelist = ["data-tracker", "data-wrapper"]
+    .concat([whileAttr])
+    .filter((item) => item);
 
   if (isAttr) {
-    for (let i = 0; i < attrWhitelist.length; i++) {
-      const attr = htmlNode.getAttribute(attrWhitelist[i]);
+    for (let i = 0; i < myAttrWhitelist.length; i++) {
+      const attr = htmlNode.getAttribute(myAttrWhitelist[i]);
       if (typeof attr === "string") {
         out = htmlNode;
       }
@@ -69,10 +80,16 @@ const htmlNodeToStr = function (htmlNode, isAttr) {
   return out;
 };
 
-const _getDateTrackerValues = (treeNode, aNode, defaultTrackerType) => {
+const _getDateTrackerValues = (treeNode, aNode, config: IConfig) => {
+  const { getTrackerType, whileAttr } = config;
+  const defaultTrackerType = getTrackerType() || null;
   const [latestNode, parentNode] = treeNode;
-  const latestTrckerValue =
-    latestNode && latestNode.getAttribute("data-tracker");
+  let latestTrckerValue = null;
+  if (latestNode) {
+    latestTrckerValue =
+      latestNode.getAttribute("data-tracker") ||
+      latestNode.getAttribute(whileAttr);
+  }
   const latestWrapperValue =
     latestNode && latestNode.getAttribute("data-wrapper");
   const parentWrapperValue =
@@ -109,21 +126,16 @@ const _getDateTrackerValues = (treeNode, aNode, defaultTrackerType) => {
   return null;
 };
 
-const addBehavior = (behavior, send, getTrackerType) => {
+const addBehavior = (behavior, send, config) => {
   const { attr: treeAttrNode, tag: treeTagNode } = behavior.treeNode;
-  const defaultTrackerType = getTrackerType() || null;
   const [aNode] = treeTagNode;
-  const trackerObject = _getDateTrackerValues(
-    treeAttrNode,
-    aNode,
-    defaultTrackerType
-  );
-  if(trackerObject) {
+  const trackerObject = _getDateTrackerValues(treeAttrNode, aNode, config);
+  if (trackerObject) {
     send(trackerObject);
   }
 };
 
-const bhEventHandler = (eventName: string, send, getTrackerType) => {
+const bhEventHandler = (eventName: string, send, config: IConfig) => {
   let lastEvent = null;
   return function (event) {
     if (!event || event === lastEvent) return;
@@ -140,33 +152,27 @@ const bhEventHandler = (eventName: string, send, getTrackerType) => {
     const behavior = {
       type: "ui.".concat(eventName), // e.g. ui.click, ui.input
       target,
-      treeNode: htmlTreeToStr(target),
+      treeNode: htmlTreeToStr(target, config),
       timestamp: Date.now(),
     };
     if (eventName === "click") {
-      addBehavior(behavior, send, getTrackerType);
+      addBehavior(behavior, send, config);
     } else if (eventName === "keypress") {
-      addBehavior(behavior, send, getTrackerType);
+      addBehavior(behavior, send, config);
     }
   };
 };
 
-export default (
-  send,
-  config: {
-    getTrackerType;
-  }
-) => {
-  const { getTrackerType = () => null } = config || {};
+export default (send, config = { getTrackerType: () => null }) => {
   if (!(window.document && window.document.addEventListener)) return;
   window.document.addEventListener(
     "click",
-    bhEventHandler("click", send, getTrackerType),
+    bhEventHandler("click", send, config),
     false
   );
   window.document.addEventListener(
     "keypress",
-    bhEventHandler("keypress", send, getTrackerType),
+    bhEventHandler("keypress", send, config),
     false
   );
 };
